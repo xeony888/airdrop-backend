45f32c51ef492228d58a3ff1a86ac38857587a3f
xeony <71081051+xeony888@users.noreply.github.com>
2025-01-13 21:08:43 -0500

    stuff

--- src/airship-ts/index.ts
+++ src/airship-ts/index.ts
@@ -4,16 +4,9 @@ import chalk from "chalk";
 import * as web3 from "@solana/web3.js";
 import {
   status,
-  logger,
-  AirdropError,
   init,
   databaseFile,
 } from "helius-airship-core";
-import ora, { Ora } from "ora";
-import { csv } from "./imports/csv";
-import { chapter2 } from "./imports/chapter-2";
-import { nft } from "./imports/nft";
-import { splToken } from "./imports/spl-token";
 import Tinypool from "tinypool";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
@@ -34,81 +27,13 @@ sqlite.exec("PRAGMA synchronous = normal;");
 const db = drizzle(sqlite);
 
 export async function main(keypair: web3.Keypair, url: string, mintAddress: string, addresses: web3.PublicKey[], amount: bigint) {
-  // const program = createCommandProgram();
   // @ts-ignore
   await init({ db });
   await handleNewAirdrop(keypair, { url }, mintAddress, addresses, amount);
-  // console.log("here");
-  // program.action(async () => {
-  //   const options = program.opts();
-  //   validateOptions(options);
-
-  //   const keypair = loadKeypair(options.keypair);
-
-  //   // Initialize the database
-  //   // @ts-ignore
-
-
-  //   const action = await selectAction();
-
-  //   switch (action) {
-  //     case "new":
-  //       await handleNewAirdrop(keypair, options);
-  //       break;
-  //     case "resume":
-  //       await resumeAirdrop(keypair, options.url);
-  //       break;
-  //     case "exit":
-  //       exitProgram();
-  //   }
-  // });
-
-  // program.parse();
 }
 
-function createCommandProgram() {
-  return new Command()
-    .name("airdrop")
-    .description("Airdrop tokens using ZK Compression")
-    .option("-k, --keypair <KEYPAIR>", "Keypair to use for the airdrop")
-    .option(
-      "-u, --url <URL>",
-      "URL for Solana's JSON RPC with ZK Compression support"
-    )
-    .version(
-      "0.8.2",
-      "-v, --version",
-      "display the version number"
-    );
-}
-
-function validateOptions(options: any) {
-  if (!options.keypair) {
-    console.log(
-      chalk.red("Please provide a keypair using the --keypair option")
-    );
-    process.exit(0);
-  }
-  if (!options.url) {
-    console.log(chalk.red("Please provide a RPC url using the --url option"));
-    process.exit(0);
-  }
-}
 
 async function handleNewAirdrop(keypair: web3.Keypair, options: any, mintAddress: string, addresses: web3.PublicKey[], amount: bigint) {
-  // await checkAndConfirmOverwrite();
-  // const tokens = await loadTokens(keypair, options.url);
-  // const mintAddress = await selectToken(keypair, tokens);
-  // const addresses = await selectRecipients(options.url);
-  // const amount = await selectAmount(tokens, mintAddress, addresses.length);
-  // await confirmAirdrop(
-  //   options.url,
-  //   keypair,
-  //   mintAddress,
-  //   addresses,
-  //   amount,
-  //   tokens
-  // );
   await createAirdropQueue(keypair, mintAddress, addresses, amount);
   await startAndMonitorAirdrop(keypair, options.url);
 }
@@ -119,7 +44,6 @@ async function createAirdropQueue(
   addresses: web3.PublicKey[],
   amount: bigint
 ) {
-  const createSpinner = ora("Creating transaction queue").start();
   try {
     await pool.run(
       {
@@ -130,23 +54,14 @@ async function createAirdropQueue(
       },
       { name: "create" }
     );
-    createSpinner.succeed("Transaction queue created");
   } catch (error) {
-    createSpinner.fail("Failed to create transaction queue");
-    logger.error("Failed to create transaction queue", error);
-    process.exit(0);
+    // logger.error("Failed to create transaction queue", error);
+    // process.exit(0);
   }
 }
 
-async function resumeAirdrop(keypair: web3.Keypair, url: string) {
-  console.log(chalk.green(`Resuming airdrop...`));
-  await startAndMonitorAirdrop(keypair, url);
-}
-
 async function startAndMonitorAirdrop(keypair: web3.Keypair, url: string) {
-  const startSpinner = ora("Starting airdrop");
   try {
-    startSpinner.start();
 
     // Worker to send transactions
     const mcSend = new MessageChannel();
@@ -158,7 +73,6 @@ async function startAndMonitorAirdrop(keypair: web3.Keypair, url: string) {
 
     mcSend.port2.on('message', (message: any) => {
       if (message.error) {
-        handleAirdropError(startSpinner, new Error(message.error));
       }
     });
 
@@ -172,13 +86,9 @@ async function startAndMonitorAirdrop(keypair: web3.Keypair, url: string) {
 
     mcPoll.port2.on('message', (message: any) => {
       if (message.error) {
-        handleAirdropError(startSpinner, new Error(message.error));
       }
     });
-
-    startSpinner.succeed("Airdrop started");
   } catch (error) {
-    handleAirdropError(startSpinner, error);
   }
 
   // const multibar = createProgressBars();
@@ -189,21 +99,7 @@ export async function getAirdropStatus() {
   return await status({ db });
 }
 
-function handleAirdropError(spinner: Ora, error: any) {
-  spinner.fail("Failed to start airdrop");
-  if (error instanceof AirdropError) {
-    console.error(chalk.red(error.message));
-  } else {
-    console.error(chalk.red(error));
-  }
-  process.exit(0);
-}
 
-function handleExitError(error: any) {
-  if (error.name === "ExitPromptError") {
-    exitProgram();
-  }
-}
 
 function exitProgram() {
   console.log(chalk.green("\nExiting..."));
